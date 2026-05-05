7d189f96a69120f5b89075016bc228db1d8a61c5
vpineda30 <vinicius.pineda.senai404@gmail.com>
2026-05-05 16:49:56 -0300

    feat(database): initialize SQLite database with User table and migration files
    - Created initial migration file to set up User table with fields: id, email, name, password, and createdAt.
    - Added unique index on email field for User table.
    - Introduced migration lock file for version control.
    - Configured Prisma client with Better SQLite3 adapter.
    - Defined Prisma schema for User model.
    - Created SQLite database file.
    - Added UserDTO interface for user data transfer objects.
    - Set up TypeScript configuration for strict type checking and module resolution.

---
+++ src/modules/User/dtos/user.dto.ts
@@ -0,0 +1,8 @@
+export interface UserDTO {
+    id: string
+    name: string
+    email: string
+    password: string
+    role: string
+    createdAt: Date
+}
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,4 @@ app.get('/', (req, res) => {
     return res.json({ message: "Hello World" });
 });
 
-app.listen(3333, () => {
-    console.log("Server is running on port 3333");
-});
+app.listen(3333, () => console.log("Server is running on port 3333")); 
